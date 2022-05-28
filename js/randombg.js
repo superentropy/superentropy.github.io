df5e91fe250b54cd6722ac7c8a94edb76aa61411
superentropy <zhangnoah@163.com>
2022-05-28 23:03:51 +0800

    Site updated: 2022-05-28 23:03:50

--- js/randombg.js
+++ js/randombg.js
@@ -1,6 +1,16 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg = [
-    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/chouxiang.jpg)"
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/0.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/1.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/2.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/3.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/4.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/5.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/6.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/7.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/8.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/9.jpg)",
+    "url(http://fastly.jsdelivr.net/gh/superentropy/my_img/10.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex = Math.ceil(Math.random() * (backimg.length - 1));
