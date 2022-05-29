abd0a0fc89a9705dc6b933770cb07471641a0f0f
superentropy <zhangnoah@163.com>
2022-05-29 18:27:46 +0800

    Site updated: 2022-05-29 18:27:45

---
+++ js/weather.js
@@ -0,0 +1,1 @@
+WIDGET={CONFIG:{modules:"01234",background:"5",tmpColor:"4A86E8",tmpSize:"16",cityColor:"FF9900",citySize:"16",aqiColor:"4A86E8",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"0px 0px 0px 0px",shadow:"0",language:"auto",fixed:"true",vertical:"center",horizontal:"center",left:"100",top:"20",key:"0a085a92b0e9454dba62e556c0a02901"}};
