7faf7da146e9034b72184b16e954e7d99ec96b3f
superentropy <zhangnoah@163.com>
2022-08-31 22:59:33 +0800

    Site updated: 2022-08-31 22:59:32

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,24 @@
-WIDGET={CONFIG:{modules:"0124",background:"1",tmpColor:"FFFFFF",tmpSize:"16",cityColor:"FFFFFF",citySize:"16",aqiColor:"FFFFFF",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"0px 0px 0px 0px",shadow:"0",language:"auto",borderRadius:"5",fixed:"true",vertical:"center",horizontal:"center",left:"100",top:"20",key:"46426b21ebea4a96badbdeaba055af46"}};
+WIDGET = {
+    "CONFIG": {
+        "modules": "0124",
+        "background": "1",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "0px 0px 0px 0px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "5",
+        "fixed": "true",
+        "vertical": "center",
+        "horizontal": "center",
+        "left": "100",
+        "top": "20",
+        "key": "46426b21ebea4a96badbdeaba055af46"
+    }
+}
